a6752f8ac451bf277abed83fae606bf0cb28cabf
Dianne Saba <dnsaba19@gmail.com>
2018-05-06 01:49:38 -0700

    Added all html and JS files for register.

---
+++ CodingKids/app/public/modules/home/home.controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('publicApp')
+        .controller('homeController', HomeController);
+
+    HomeController.$inject = ['$scope'];
+
+    function HomeController($scope) {
+        var vm = this;
+        vm.$scope = $scope;
+
+        vm.$onInit = _onInit;
+
+        function _onInit() {
+            console.log('homeController onInit');
+        }
+    }
+})();
---
+++ CodingKids/app/public/modules/routes/app.routes.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+    var myApp = angular.module('publicApp.routes', []);
+    myApp.config(function ($stateProvider, $locationProvider, $urlProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false,
+        });
+        $urlProvider.otherwise('/home');
+
+        $stateProvider
+
+            .state('home', {
+                name: 'home',
+                url: '/home',
+                templateUrl: '/app/public/modules/home/home.html',
+                controller: 'homeController as homeCtrl'
+            });
+    });
+})();
+
+
+
+
